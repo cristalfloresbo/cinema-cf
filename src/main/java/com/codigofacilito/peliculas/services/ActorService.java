c6cfcf0825bb3f189c3ef13873dc4341b8e6f4c0
Cristal Flores <cristalflores.bo@gmail.com>
2024-09-17 23:43:32 -0400

    Creando Form Pelicula Pt.3

--- src/main/java/com/codigofacilito/peliculas/services/ActorService.java
+++ src/main/java/com/codigofacilito/peliculas/services/ActorService.java
@@ -2,6 +2,7 @@ package com.codigofacilito.peliculas.services;
 
 import com.codigofacilito.peliculas.dao.IActorRepository;
 import com.codigofacilito.peliculas.entities.Actor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -9,6 +10,7 @@ import java.util.List;
 @Service
 public class ActorService implements IActorService {
 
+    @Autowired
     private IActorRepository repository;
 
     @Override
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -4,6 +4,6 @@ INSERT INTO GENEROS(NOMBRE) VALUES ('Terror');
 INSERT INTO GENEROS(NOMBRE) VALUES ('Drama');
 INSERT INTO GENEROS(NOMBRE) VALUES ('Fantasia');
 
-INSERT INTO ACTORES(NOMBRE, URL_IMAGEN) VALUES ('Hugh Jackman', 'https://pbs.twimg.com/profile_images/1302962150302982146/NTb6iGpC_400x400.jpg');
-INSERT INTO ACTORES(NOMBRE, URL_IMAGEN) VALUES ('Patrick Stewart', 'https://media.ambito.com/p/d5c1807c0a638b9cbf6a9b654da6842b/adjuntos/239/imagenes/040/360/0040360285/messi-stewart-1jpg.jpg')
-INSERT INTO ACTORES(NOMBRE, URL_IMAGEN) VALUES ('Keanu Reeves', 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/33/Reuni%C3%A3o_com_o_ator_norte-americano_Keanu_Reeves_%2846806576944%29_%28cropped%29.jpg/330px-Reuni%C3%A3o_com_o_ator_norte-americano_Keanu_Reeves_%2846806576944%29_%28cropped%29.jpg');
+INSERT INTO ACTORES(NOMBRE, URL_IMAGEN) VALUES ('Hugh Jackman', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSmKoccv2BORVHLsmakaOlZTSk6kd_0eGodVA&s');
+INSERT INTO ACTORES(NOMBRE, URL_IMAGEN) VALUES ('Patrick Stewart', 'https://resizing.flixster.com/-XZAfHZM39UwaGJIFWKAE8fS0ak=/v3/t/assets/50226_v9_bd.jpg')
+INSERT INTO ACTORES(NOMBRE, URL_IMAGEN) VALUES ('Keanu Reeves', 'https://cdn.britannica.com/11/215011-050-3127A07E/American-actor-Keanu-Reeves-2014.jpg');
